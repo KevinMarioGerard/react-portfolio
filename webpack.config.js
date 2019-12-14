64d9cb59da7e8a19e4de4b690de32a4462c085ae
Kevin Mario Gerard <kevinmariogerard@gmail.com>
2019-12-15 03:41:30 +0530

    Prettier Setup
    Setup prettier

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist/"),
         publicPath: "/dist/",
-        filename: "bundle.js"
+        filename: "js/bundle.js"
     },
 
     module: {
