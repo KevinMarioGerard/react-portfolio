591bcf92ead8924285a0aebda29abf9f014c51f3
Kevin Mario Gerard <kevinmariogerard@gmail.com>
2019-12-15 19:08:58 +0530

    Sass setup

--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,14 @@ module.exports = {
                 loader: "ts-loader"
             },
             {
-                test: /\.(js|jsx)$/,
+                test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: "babel-loader",
                 options: { presets: ["@babel/env"] }
             },
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     },
