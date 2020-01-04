36907f225527113dd37cd2f1b0ecbb2c03781b7c
Kevin Mario Gerard <kevinmariogerard@gmail.com>
2020-01-04 23:29:32 +0530

    Updated Nav Bar

--- src/app/navbar/NavBar.tsx
+++ src/app/navbar/NavBar.tsx
@@ -6,10 +6,10 @@ const NavBar = () => {
         <Nav>
             <Nav.Header header="Kevin Mario Gerard" />
             <Nav.ItemsList>
-                <Nav.Item label="Home" id="home"/>
-                <Nav.Item label="About" id="about"/>
-                <Nav.Item label="Tech Skills" id="techSkills"/>
-                <Nav.Item label="Contact" id="contact"/>
+                <Nav.Item label="Home" id="home" />
+                <Nav.Item label="About" id="about" />
+                <Nav.Item label="Tech Skills" id="techSkills" />
+                <Nav.Item label="Contact" id="contact" />
             </Nav.ItemsList>
         </Nav>
     );
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,57 +1,45 @@
 import * as React from 'react';
 
+type NavHeaderProps = {
+    header: string;
+};
+
+const NavHeader: React.FC<NavHeaderProps> = ({ header = '' }) => (
+    <div className="nav__header">
+        <header>{header}</header>
+    </div>
+);
+
+NavHeader.displayName = 'Nav.Header';
+
+const NavItemsList: React.FC = props => <div className="nav__items-list">{props.children}</div>;
+
+NavItemsList.displayName = 'Nav.ItemsList';
+
 type NavItemProps = {
     label: string;
     id: string;
 };
 
-type NavHeaderProps = {
-    header: string;
+const NavItem: React.FC<NavItemProps> = ({ label = '', id = '' }) => (
+    <div className="nav__item">
+        <a href={`#${id}`}>{label}</a>
+    </div>
+);
+
+NavItem.displayName = 'NavItem';
+
+type Statics = {
+    Header: typeof NavHeader;
+    Item: typeof NavItem;
+    ItemsList: typeof NavItemsList;
 };
 
-type NavSubComponents = {
-    Header: React.FC<NavHeaderProps>;
-    Item: React.FC<NavItemProps>;
-    ItemsList: React.FC;
-};
-
-const Nav: React.FC & NavSubComponents = props => (
+const Nav: React.FC & Statics = props => (
     <nav className="nav__bar">
         <div className="container">{props.children}</div>
     </nav>
 );
-
-const NavHeader: React.FC<NavHeaderProps> = props => {
-    const { header = "" } = props;
-
-    return (
-        <div className="nav__header">
-            <header>{header}</header>
-        </div>
-    )
-};
-
-NavHeader.displayName = "Nav.Header";
-
-const NavItemsList: React.FC = props => (
-    <div className="nav__items-list">
-        {props.children}
-    </div>
-);
-
-NavItemsList.displayName = "Nav.ItemsList";
-
-const NavItem: React.FC<NavItemProps> = props => {
-    const { label = '', id = "" } = props;
-
-    return (
-        <div className="nav__item">
-            <a href={`#${id}`}>{label}</a>
-        </div>
-    );
-};
-
-NavItem.displayName = 'NavItem';
 
 Nav.Header = NavHeader;
 Nav.ItemsList = NavItemsList;
