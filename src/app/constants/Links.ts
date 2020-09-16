96822ce7bea929c4c51b715a33a91b2e7f860b82
KevinMarioGerard <kevinmariogerard@gmail.com>
2020-09-17 01:40:00 +0530

    Added style variables
    + Refactored to add scss variables
    + Added Link to nav footer

--- src/app/constants/Links.ts
+++ src/app/constants/Links.ts
@@ -2,4 +2,6 @@ export default {
     linkedInUrl: 'https://www.linkedin.com/in/kevinmariogerard/',
 
     githubUrl: 'https://github.com/KevinMarioGerard',
+
+    repoUrl: 'https://github.com/KevinMarioGerard/react-portfolio',
 };
